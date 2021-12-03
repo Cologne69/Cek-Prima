70939b21efe86a45a20fc72b43e42c924a1a1bf5
Cologne69 <kleanosiauw@student.ciputra.ac.id>
2021-12-03 12:22:21 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Cek_Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan angka: ");
+            int input = int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int i = 2; i <= input/2; i++)
+            {
+                if (input % i == 0)
+                {
+                    Console.Write("Angka bukan prima");
+                    counter = 1;
+                    break;
+                }
+            }
+            if (counter == 0)
+                Console.Write("Angka prima");
+        }
+    }
+}
